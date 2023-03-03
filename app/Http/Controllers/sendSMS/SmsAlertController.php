46b6683da6f7f5a248c7384265931806dc099d8e
harry-advanta <haroldkerry@advantasms.com>
2023-03-03 14:35:44 +0300

    Changes to sendSMS API

--- app/Http/Controllers/sendSMS/SmsAlertController.php
+++ app/Http/Controllers/sendSMS/SmsAlertController.php
@@ -11,39 +11,61 @@ use Nixon\MobileNumber\MobileNumber;
 
 class SmsAlertController extends Controller
 {
-    public function sendSMS($data){
-
+   public function sendSMS($data){
 
 
         $network = MobileNumber::getNetwork($data);
 
-        if($network == "Safaricom"){
+        switch($network){
 
+            case 'Safaricom':
+                return $this->send($data,'20133');
+                break;
+            case 'Airtel':
+                return $this->send($data,'LOVENRBFEST ');
+                break;
 
-        $client = new Client();
-       $response = $client->post(
-        'https://quicksms.advantasms.com/api/services/sendsms/',
-        [
-            'headers' => [
-                'Content-Type' => 'application/json',
-            ],
-            'json' => [
-                'apikey' => '64f46f5e7d59ab7d8f56b2797e99dd68',
-                'partnerID' => '6911',
-                'mobile' => $data,
-                'message' => 'Thank you for registering for the Love Nairobi Festival Launch',
-                'shortcode' => '20133',
-                'pass_type' => 'plain',
-            ],
-        ]
-    );
-
+            default:
+                return $this->send($data, 'null');
+                break;
+        }
+ 
     
+    }
 
-    return $response;
 
-}
+    public function send($data,$senderID){
+
+        if($senderID == null){
+            return;
+        }else{
+            $client = new Client();
+            $response = $client->post(
+            'https://quicksms.advantasms.com/api/services/sendsms/',
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => [
+                    'apikey' => '64f46f5e7d59ab7d8f56b2797e99dd68',
+                    'partnerID' => '6911',
+                    'mobile' => $data,
+                    'message' => 'Thank you for registering for the Love Nairobi Festival Launch',
+                    'shortcode' => $senderID,
+                    'pass_type' => 'plain',
+                ],
+            ]
+        );
+    
+        return $response;
+        }
+
+           
+    
+    }
     
 
     }
-}
+
+
+
